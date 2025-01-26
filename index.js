cc654d32136cef4ef52c292eae46e480a286ef04
MirazZim <miraz.zim.38@gmail.com>
2025-01-26 12:48:33 +0600

    Server now running without any error

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const users = [
+    {id: 1, name:'Sabana', email: 'Miraz@lakar.com' },
+    {id: 2, name:'Labana', email: 'Miraz@lakar.com' },
+    {id: 3, name:'Kabana', email: 'Miraz@lakar.com' },
+    {id: 4, name:'Eabana', email: 'Miraz@lakar.com' },
+    {id: 5, name:'Pabana', email: 'Miraz@lakar.com' },
+]
 
 app.get('/', (req, res ) =>{
     res.send('Users management server is running')
 
+});
+
+app.get('/users',(req,res)=>{
+res.send(users)
 })
 
 
